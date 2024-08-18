cd41a25533b712bf6b58b7ff4fd3699bc4f98875
mahyar1284 <mahyarshokraeian@live.com>
2024-08-18 21:44:28 +0330

    data retrival from mock server implemented

--- src/pages/PumpSettings.tsx
+++ src/pages/PumpSettings.tsx
@@ -11,15 +11,13 @@ interface SchedulerItemProps {
   enabled: boolean;
   on: boolean;
 }
-interface TimeAndWeekday {
-  time: string;
-  weekday: string;
-}
 
 const PumpSettings: React.FC = () => {
   const [schedulerItems, setSchedulerItems] = useState<SchedulerItemProps[]>(
     []
   );
+  const [currentTime, setCurrentTime] = useState<string>("");
+  const [currentWeekday, setCurrentWeekday] = useState<string>("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -34,13 +32,33 @@ const PumpSettings: React.FC = () => {
         console.error("Failed to fetch data:", error);
       }
     };
-
     fetchData();
+    const intervalId = setInterval(fetchData, 5000); // Update every 5 second
+
+    return () => clearInterval(intervalId);
   }, []);
 
   useEffect(() => {
-    console.log("Scheduler items updated:", schedulerItems);
-  }, [schedulerItems]);
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/getCurrentTime");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+
+        setCurrentTime(data.time);
+        setCurrentWeekday(data.weekday);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+      }
+    };
+
+    fetchData();
+    const intervalId = setInterval(fetchData, 60000); // Update every minute
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   const handleChange = (newItems: SchedulerItemProps[]) => {
     setSchedulerItems(newItems);
@@ -55,7 +73,12 @@ const PumpSettings: React.FC = () => {
           <h1 className="page-header text-center">Pump Settings</h1>
         </div>
         <div className="d-flex justify-content-center align-items-center">
-          <SchedulerListGroup items={schedulerItems} onChange={handleChange} />
+          <SchedulerListGroup
+            items={schedulerItems}
+            onChange={handleChange}
+            currentTime={currentTime}
+            currentWeekday={currentWeekday}
+          />
         </div>
       </div>
     </>
