eb69869cc5c12d2159d2e1fb1838234c85a2bf23
mahyar1284 <mahyarshokraeian@live.com>
2025-02-09 18:27:33 +0330

    GPIO manager added

---
+++ src/Components/GPIOItem.tsx
@@ -0,0 +1,159 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { PencilSquare } from "react-bootstrap-icons";
+import { FloppyFill } from "react-bootstrap-icons";
+import {Event} from "./EventManager";
+
+export interface GPIOItemProps {
+  id: number;
+  mode: number;
+  event_id: number;
+  status: boolean;
+  name: string;
+}
+
+interface GPIOItemComponentProps {
+  item: GPIOItemProps;
+  eventList: Event[];
+  onRemove: (pin: number) => void;
+  onSave: (item: GPIOItemProps) => void;
+}
+
+const GPIOItem: React.FC<GPIOItemComponentProps> = ({
+  item,
+  eventList,
+  onRemove,
+  onSave
+}) => {
+  const handleRemove = () => {
+    onRemove(localItem.id);
+  };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLocalItem(prevItem => ({
+      ...prevItem,
+      name: e.target.value
+    }));
+  };
+
+  const handleEventChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setLocalItem(prevItem => ({
+      ...prevItem,
+      event_id: Number(e.target.value)
+    }));
+  }; 
+
+  const handleModeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setLocalItem(prevItem => ({
+      ...prevItem,
+      mode: Number(e.target.value)
+    }));
+  }; 
+
+  const handlePinChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLocalItem(prevItem => ({
+      ...prevItem,
+      id: Number(e.target.value)
+    }));
+  }; 
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [localItem, setLocalItem] = useState(item);
+  const handleEditClick = () => {
+    if (isEditing) onSave(localItem);
+    setIsEditing(!isEditing);
+  };
+
+  return (
+    <div
+      className={`list-group-item ${
+        isEditing ? "border-primary" : ""
+      } d-flex flex-column align-items-start m-2 border p-2 rounded p-2`}>
+      <div className="d-flex flex-column align-items-center w-100 mb-3">
+        <div className="d-flex align-items-center w-100 mb-1">
+          <label className="text-muted w-auto"> Name: </label>
+          <input
+            title="name"
+            type="text"
+            className={`w-auto mx-1 ${isEditing ? "form-control" : "form-control-plaintext text-muted"}`}
+            value={localItem.name}
+            onChange={handleNameChange}
+            disabled={!isEditing} 
+          />
+        </div>
+        <div className="d-flex align-items-center w-100 mb-1">
+          <label className="text-muted w-auto"> Pin: </label>
+          <input
+            title="name"
+            type="text"
+            className={`w-auto mx-1 ${isEditing ? "form-control" : "form-control-plaintext text-muted"}`}
+            value={localItem.id}
+            onChange={handlePinChange}
+            disabled={!isEditing} 
+          />
+        </div>
+        <div className="d-flex align-items-center w-100 mb-1">
+          <label className="text-muted w-auto mx-2"> Mode: </label>
+          <select
+            title="mode"
+            className={`w-auto mx-1 ${isEditing ? "form-control" : "form-control-plaintext text-muted"}`}
+            value={localItem.mode}
+            onChange={handleModeChange}
+            disabled={!isEditing}
+          >
+            <option value={0}>Input</option>
+            <option value={1}>Output</option>
+          </select>
+        </div>
+        <div className="d-flex align-items-center w-100 mb-1">
+          <label className="text-muted w-auto mx-2"> {localItem.mode === 1 ? "Listen to:" : "Broadcast to:"} </label>
+          <select
+            title="eventID"
+            className={`w-auto mx-1 ${isEditing ? "form-control" : "form-control-plaintext text-muted"}`}
+            value={localItem.event_id}
+            onChange={handleEventChange}
+            disabled={!isEditing}
+          >
+            <option value={-1}>No events assigned!</option>
+            {eventList.map((event) => (
+              <option key={event.id} value={event.id}>
+                {event.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      
+      <div className="d-flex align-items-center w-100 mb-3">
+        <div className="d-flex flex-column">
+          <button
+            className={`btn btn-primary rounded-circle `}
+            onClick={handleEditClick}
+          >
+            {" "}
+            <div hidden={isEditing}>
+              <PencilSquare />
+            </div>
+            <div hidden={!isEditing}>
+              <FloppyFill />
+            </div>
+          </button>
+        </div>
+      </div>
+      <div className="d-flex w-100">
+        <button className="btn btn-danger w-100 mx-1" onClick={handleRemove}>
+          Remove
+        </button>
+        <button
+          className={`btn w-100 mx-1 ${
+            item.status ? "btn-success" : "btn-secondary"
+          }`} onClick={() => { if (localItem.event_id === -1) { localItem.status = !localItem.status; onSave(localItem); }}}
+        >
+          {item.status ? "I/O: High" : "I/O: Low"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default GPIOItem;
---
+++ src/pages/PinManagerPage.tsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import GPIOListGroup from "../Components/GPIOListGroup";
+import TitleBar from "../Components/TitleBar";
+import axios from 'axios';
+import { GPIOItemProps } from "../Components/GPIOItem";
+import { Event } from "../Components/EventManager";
+
+const PinManagerPage: React.FC = () => {
+  const [GPIOItems, setGPIOItems] = useState<GPIOItemProps[]>([]);
+  const [events, setEvents] = useState<Event[]>([]);
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  
+  useEffect(() => {
+    axios.get<Event[]>('http://localhost:3000/getEventList').then((response) => {
+      setEvents(response.data);
+    });
+  }, []);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get<GPIOItemProps[]>('http://localhost:3000/getGPIOList');
+      setGPIOItems(response.data);
+    } catch (error) {
+      console.error('Error fetching schedule list:', error);
+    }
+  };
+
+  const createGPIO = async (newIO: GPIOItemProps) => {
+    const newIOData = { ...newIO };
+    try {
+      await axios.post('http://localhost:3000/createGPIO', newIOData);
+    } catch (error) {
+      console.error('Error creating GPIO:', error);
+    }
+    fetchList(); //get updated list
+  };
+
+  const deleteGPIO = async (id: number) => {
+    try {
+      await axios.post('http://localhost:3000/deleteGPIO', { id });
+    } catch (error) {
+      console.error('Error deleting GPIO:', error);
+    }
+    fetchList(); //get updated list
+  };
+
+  const modifyGPIO = async (updatedItem: GPIOItemProps) => {
+    try {
+      await axios.post('http://localhost:3000/modifyGPIO', {...updatedItem });
+    } catch (error) {
+      console.error('Error modifying GPIO:', error);
+    }
+    fetchList(); // get updated list
+  };
+
+  return (
+    <>
+      <div className="flex-d align-items-center">
+        <TitleBar title="Manage I/Os" />
+        <div className="d-flex justify-content-center align-items-center">
+          <GPIOListGroup
+            items={GPIOItems}
+            onSave={modifyGPIO}
+            onRemove={deleteGPIO}
+            onCreate={createGPIO}
+            eventList={events}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PinManagerPage;
