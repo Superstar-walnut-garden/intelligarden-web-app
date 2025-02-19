2f58cd268fb5605ca27ae75b2a90c49d785e0f0a
mahyar1284 <mahyarshokraeian@live.com>
2025-02-19 23:00:24 +0330

    configure Vite to add development server proxy for forwarding API requests to esp32

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import React from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  return{
+    plugins: [React()],
+    server: {
+      proxy: mode === 'development' ? {
+        '/api/': {
+          target: 'http://192.168.4.1/', // esp32 hotspot server ip
+          changeOrigin: true,
+          secure: false,
+        },
+      } : undefined,
+    },
+  };
+});
