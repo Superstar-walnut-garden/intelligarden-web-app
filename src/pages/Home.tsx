951020381b826068263e7868cbe80c419c758a71
mahyar1284 <mahyarshokraeian@live.com>
2025-03-06 01:22:41 +0330

    add Thermostat Manager Icon to Homescreen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,6 +10,7 @@ const Home = () => {
         <Widget title="WiFi Settings" info="Change WiFi or Hotspot settings" link="/wifisettings" bgColor="#ffebee" />
         <Widget title="Manage Schedulers" info="Schedule your irrigation, lighting and etc..." link="/pumpsettings" bgColor="#e3f2fd" />
         <Widget title="Temperature Sensor Setup" info="Setup your sensors" link="/TempSensorSettings" bgColor="#e8f5e9" />
+        <Widget title="Manage Thermostats" info="Setup your Temperature Controllers" link="/thermostat-manager" bgColor="#ffaaaa" />
         <Widget title="Firebase Settings" info="Configure Firebase" link="/FirebaseSettings" bgColor="#fff3e0" />
         <Widget title="Water Sensor Settings" info="Pond Inlet Water Data Logging" link="/" bgColor="#a3dafd" />
         <Widget title="Display Settings" info="Config Display Properties" link="/DisplaySettings" bgColor="#f3e5f5" />
