896dee30d123445d0ae2fea50996cc3fb40fe9d5
mahyar1284 <mahyarshokraeian@live.com>
2025-06-13 16:28:44 +0330

    app compression and size reduction

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,29 @@
-import { defineConfig } from 'vite'
-import React from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import React from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig(({ mode }) => {
-  return{
-    plugins: [React()],
+  return {
+    plugins: [
+      React(),
+      viteCompression({
+        algorithm: "gzip",
+        ext: ".gz",
+        filter: () => true, // Compress all files
+        deleteOriginFile: true, // Removes original files after compression
+      }),
+    ],
     server: {
-      proxy: mode === 'development' ? {
-        '/api/': {
-          target: 'http://192.168.1.185/', // esp32 hotspot server ip
-          changeOrigin: true,
-          secure: false,
-        },
-      } : undefined,
+      proxy:
+        mode === "development"
+          ? {
+              "/api/": {
+                target: "http://192.168.4.1/", // esp32 hotspot server ip
+                changeOrigin: true,
+                secure: false,
+              },
+            }
+          : undefined,
     },
   };
 });
