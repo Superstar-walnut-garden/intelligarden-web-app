86835197a00d714822c5df6cb69ddb995d66c768
mahyar1284 <mahyarshokraeian@live.com>
2025-06-30 17:37:55 +0330

    Add: ItemTitle

---
+++ src/Components/ItemTitle.tsx
@@ -0,0 +1,37 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+
+interface ItemTitleProps {
+  title?: string;
+  isEditing?: boolean;
+  onTextChange?: (text: string) => void;
+}
+
+const ItemTitle: React.FC<ItemTitleProps> = ({
+  title = "Unnamed Title",
+  isEditing = false,
+  onTextChange,
+}) => {
+  const [titleText, setTitleText] = useState(title);
+  return (
+    <>
+      <div className="d-flex align-items-center w-100 mb-1 mx-auto text-center justify-content-center bg-secondary rounded-top">
+        <input
+          title="name"
+          type="text"
+          className={`w-auto mx-1 bg-secondary border-0 text-white text-center fw-bold ${
+            isEditing ? "form-control bg-primary" : "form-control-plaintext"
+          }`}
+          value={titleText}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitleText(e.target.value);
+            if (onTextChange) onTextChange(String(e.target.value));
+          }}
+          disabled={!isEditing}
+        />
+      </div>
+    </>
+  );
+};
+
+export default ItemTitle;
