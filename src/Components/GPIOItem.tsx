204923b2cf702199b67bec19247f0ea3cf4f59ba
mahyar1284 <mahyarshokraeian@live.com>
2025-06-30 23:04:08 +0330

    Enhance: GPIOItem UI

--- src/Components/GPIOItem.tsx
+++ src/Components/GPIOItem.tsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { PencilSquare } from "react-bootstrap-icons";
 import { FloppyFill } from "react-bootstrap-icons";
-import {Event} from "../Components/EventItem";
+import { Event } from "../Components/EventItem";
+import ItemTitle from "./ItemTitle";
 
 export interface GPIOItemProps {
   id: number;
@@ -23,39 +24,39 @@ const GPIOItem: React.FC<GPIOItemComponentProps> = ({
   item,
   eventList,
   onRemove,
-  onSave
+  onSave,
 }) => {
   const handleRemove = () => {
     onRemove(localItem.id);
   };
 
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLocalItem(prevItem => ({
+  const handleNameChange = (text: string) => {
+    setLocalItem((prevItem) => ({
       ...prevItem,
-      name: e.target.value
+      name: text,
     }));
   };
 
   const handleEventChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setLocalItem(prevItem => ({
+    setLocalItem((prevItem) => ({
       ...prevItem,
-      event_id: Number(e.target.value)
+      event_id: Number(e.target.value),
     }));
-  }; 
+  };
 
   const handleModeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setLocalItem(prevItem => ({
+    setLocalItem((prevItem) => ({
       ...prevItem,
-      mode: Number(e.target.value)
+      mode: Number(e.target.value),
     }));
-  }; 
+  };
 
   const handlePinChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLocalItem(prevItem => ({
+    setLocalItem((prevItem) => ({
       ...prevItem,
-      id: Number(e.target.value)
+      id: Number(e.target.value),
     }));
-  }; 
+  };
 
   const [isEditing, setIsEditing] = useState(false);
   const [localItem, setLocalItem] = useState(item);
@@ -68,35 +69,34 @@ const GPIOItem: React.FC<GPIOItemComponentProps> = ({
     <div
       className={`list-group-item ${
         isEditing ? "border-primary" : ""
-      } d-flex flex-column align-items-start m-2 border p-2 rounded p-2`}>
-      <div className="d-flex flex-column align-items-center w-100 mb-3">
-        <div className="d-flex align-items-center w-100 mb-1">
-          <label className="text-muted w-auto"> Name: </label>
-          <input
-            title="name"
-            type="text"
-            className={`w-auto mx-1 ${isEditing ? "form-control" : "form-control-plaintext text-muted"}`}
-            value={localItem.name}
-            onChange={handleNameChange}
-            disabled={!isEditing} 
-          />
-        </div>
+      } d-flex flex-column align-items-start m-2 border rounded p-0`}
+    >
+      <ItemTitle
+        title={localItem.name}
+        isEditing={isEditing}
+        onTextChange={handleNameChange}
+      />
+      <div className="d-flex flex-column align-items-center w-100 m-2 mb-3">
         <div className="d-flex align-items-center w-100 mb-1">
           <label className="text-muted w-auto"> Pin: </label>
           <input
             title="name"
             type="text"
-            className={`w-auto mx-1 ${isEditing ? "form-control" : "form-control-plaintext text-muted"}`}
+            className={`w-auto mx-1 ${
+              isEditing ? "form-control" : "form-control-plaintext text-muted"
+            }`}
             value={localItem.id}
             onChange={handlePinChange}
-            disabled={!isEditing} 
+            disabled={!isEditing}
           />
         </div>
         <div className="d-flex align-items-center w-100 mb-1">
           <label className="text-muted w-auto mx-2"> Mode: </label>
           <select
             title="mode"
-            className={`w-auto mx-1 ${isEditing ? "form-control" : "form-control-plaintext text-muted"}`}
+            className={`w-auto mx-1 ${
+              isEditing ? "form-control" : "form-control-plaintext text-muted"
+            }`}
             value={localItem.mode}
             onChange={handleModeChange}
             disabled={!isEditing}
@@ -106,10 +106,15 @@ const GPIOItem: React.FC<GPIOItemComponentProps> = ({
           </select>
         </div>
         <div className="d-flex align-items-center w-100 mb-1">
-          <label className="text-muted w-auto mx-2"> {localItem.mode === 1 ? "Listen to:" : "Broadcast to:"} </label>
+          <label className="text-muted w-auto mx-2">
+            {" "}
+            {localItem.mode === 1 ? "Listen to:" : "Broadcast to:"}{" "}
+          </label>
           <select
             title="eventID"
-            className={`w-auto mx-1 ${isEditing ? "form-control" : "form-control-plaintext text-muted"}`}
+            className={`w-auto mx-1 ${
+              isEditing ? "form-control" : "form-control-plaintext text-muted"
+            }`}
             value={localItem.event_id}
             onChange={handleEventChange}
             disabled={!isEditing}
@@ -123,8 +128,8 @@ const GPIOItem: React.FC<GPIOItemComponentProps> = ({
           </select>
         </div>
       </div>
-      
-      <div className="d-flex align-items-center w-100 mb-3">
+
+      <div className="d-flex align-items-center w-100 m-2 mb-3">
         <div className="d-flex flex-column">
           <button
             className={`btn btn-primary rounded-circle `}
@@ -147,7 +152,13 @@ const GPIOItem: React.FC<GPIOItemComponentProps> = ({
         <button
           className={`btn w-100 mx-1 ${
             item.status ? "btn-success" : "btn-secondary"
-          }`} onClick={() => { if (localItem.event_id === -1) { localItem.status = !localItem.status; onSave(localItem); }}}
+          }`}
+          onClick={() => {
+            if (localItem.event_id === -1) {
+              localItem.status = !localItem.status;
+              onSave(localItem);
+            }
+          }}
         >
           {item.status ? "I/O: High" : "I/O: Low"}
         </button>
