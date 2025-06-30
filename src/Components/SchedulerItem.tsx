ac6f44a85095a448e672416f24c67999030821c8
mahyar1284 <mahyarshokraeian@live.com>
2025-06-30 23:04:31 +0330

    Enhance: SchedulerItem UI

--- src/Components/SchedulerItem.tsx
+++ src/Components/SchedulerItem.tsx
@@ -3,8 +3,9 @@ import WeekSelector from "./WeekSelector";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { PencilSquare } from "react-bootstrap-icons";
 import { FloppyFill } from "react-bootstrap-icons";
-import {Event} from "../Components/EventItem";
+import { Event } from "../Components/EventItem";
 import EventPicker from "../Components/EventPicker";
+import ItemTitle from "./ItemTitle";
 
 export interface SchedulerItemProps {
   id: number;
@@ -31,12 +32,12 @@ const SchedulerItem: React.FC<SchedulerItemComponentProps> = ({
   item,
   eventList,
   onRemove,
-  onSave
+  onSave,
 }) => {
   const handleWeekSelectorChange = (weekday: string) => {
-    setLocalItem(prevItem => ({
+    setLocalItem((prevItem) => ({
       ...prevItem,
-      weekday: weekday
+      weekday: weekday,
     }));
   };
   const handleRemove = () => {
@@ -44,69 +45,78 @@ const SchedulerItem: React.FC<SchedulerItemComponentProps> = ({
   };
 
   const handleStartTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLocalItem(prevItem => ({
+    setLocalItem((prevItem) => ({
       ...prevItem,
-      start: e.target.value
+      start: e.target.value,
     }));
   };
 
-  const handleDurationChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLocalItem(prevItem => ({
+  const handleDurationChange = (duration: string) => {
+    setLocalItem((prevItem) => ({
       ...prevItem,
-      duration: e.target.value
+      duration: duration,
     }));
   };
 
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLocalItem(prevItem => ({
+  const handleNameChange = (text: string) => {
+    setLocalItem((prevItem) => ({
       ...prevItem,
-      name: e.target.value
+      name: text,
     }));
   };
 
   const handleEventChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setLocalItem(prevItem => ({
+    setLocalItem((prevItem) => ({
       ...prevItem,
-      event_id: Number(e.target.value)
+      event_id: Number(e.target.value),
     }));
-  }; 
+  };
   const handleModeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setLocalItem(prevItem => ({
+    setLocalItem((prevItem) => ({
       ...prevItem,
-      mode: String(e.target.value)
+      mode: String(e.target.value),
     }));
-  }; 
+  };
 
   const [isEditing, setIsEditing] = useState(false);
   const [localItem, setLocalItem] = useState(item);
   const handleEditClick = () => {
-    if (isEditing) onSave(localItem);
-    setIsEditing(!isEditing);
+    if (isEditing) {
+      if (
+        localItem.duration === "" ||
+        /^([01]\d|2[0-3]):([0-5]\d)$/.test(localItem.duration)
+      ) {
+        onSave(localItem);
+        setIsEditing(false);
+      } else
+        alert(
+          "invalid time format in 'Duration' field! Please enter in a format like 02:45"
+        );
+    } else setIsEditing(true);
   };
 
   return (
     <div
       className={`list-group-item ${
         isEditing ? "border-primary" : ""
-      } d-flex flex-column align-items-start m-2 border p-2 rounded p-2`}>
+      } d-flex flex-column align-items-start m-2 border p-0 rounded`}
+    >
+      <ItemTitle
+        title={localItem.name}
+        isEditing={isEditing}
+        onTextChange={handleNameChange}
+      />
       <div className="d-flex flex-column align-items-center w-100 mb-3">
         <div className="d-flex align-items-center w-100 mb-1">
           <label className="text-muted w-auto mx-2"> ID: {item.id}</label>
-          <label className="text-muted w-auto"> Name: </label>
-          <input
-            title="name"
-            type="text"
-            className={`w-auto mx-1 ${isEditing ? "form-control" : "form-control-plaintext text-muted"}`}
-            value={localItem.name}
-            onChange={handleNameChange}
-            disabled={!isEditing} 
-          />
         </div>
         <div className="d-flex align-items-center w-100 mb-1">
           <label className="text-muted w-auto mx-2"> Event: </label>
           <select
             title="eventID"
-            className={`w-auto mx-1 ${isEditing ? "form-control" : "form-control-plaintext text-muted"}`}
+            className={`w-auto mx-1 ${
+              isEditing ? "form-control" : "form-control-plaintext text-muted"
+            }`}
             value={localItem.event_id}
             onChange={handleEventChange}
             disabled={!isEditing}
@@ -119,30 +129,51 @@ const SchedulerItem: React.FC<SchedulerItemComponentProps> = ({
             ))}
           </select>
         </div>
+        <div className={`d-flex align-items-center w-100 mb-1 `}>
+          <label className="text-muted w-auto mx-2"> Skip Event: </label>
+          <EventPicker
+            value={localItem.skipEvent_id}
+            onChange={(id: number) => {
+              setLocalItem((prevItem) => ({
+                ...prevItem,
+                skipEvent_id: id,
+              }));
+            }}
+            disabled={!isEditing}
+          />
+        </div>
         <div className="d-flex align-items-center w-100 mb-1">
           <label className="text-muted w-auto mx-2"> Mode: </label>
           <select
             title="mode"
-            className={`w-auto mx-1 ${isEditing ? "form-control" : "form-control-plaintext text-muted"}`}
+            className={`w-auto mx-1 ${
+              isEditing ? "form-control" : "form-control-plaintext text-muted"
+            }`}
             value={localItem.mode}
             onChange={handleModeChange}
             disabled={!isEditing}
           >
-            <option value={"monthly"} disabled>Monthly(not implemented)</option>
+            <option value={"monthly"} disabled>
+              Monthly(not implemented)
+            </option>
             <option value={"weekly"}>Weekly</option>
-            <option value={"daily"} disabled>Daily(not implemented)</option>
+            <option value={"daily"} disabled>
+              Daily(not implemented)
+            </option>
             <option value={"hourly"}>Hourly</option>
           </select>
         </div>
       </div>
-      
+
       <div className="d-flex align-items-center w-100 mb-3">
         <div className="d-flex flex-column mx-3">
-          <label>{localItem.mode === "weekly" ? 'Start:' : 'Interval'} </label>
+          <label>{localItem.mode === "weekly" ? "Start:" : "Interval"} </label>
           <input
             title="Start"
             type="time"
-            className={`${isEditing ? "form-control" : "form-control-plaintext text-muted"}`}
+            className={`${
+              isEditing ? "form-control" : "form-control-plaintext text-muted"
+            }`}
             value={localItem.start}
             onChange={handleStartTimeChange}
             disabled={!isEditing}
@@ -152,19 +183,27 @@ const SchedulerItem: React.FC<SchedulerItemComponentProps> = ({
           <label>Duration: </label>
           <input
             title="Duration"
-            type="time"
-            className={`${isEditing ? "form-control" : "form-control-plaintext text-muted"}`}
+            type="input"
+            maxLength={5}
+            size={5}
+            placeholder="HH:MM"
+            className={`flex-shrink w-auto ${
+              isEditing ? "form-control" : "form-control-plaintext text-muted"
+            }`}
             value={localItem.duration}
-            onChange={handleDurationChange}
+            onChange={(e) => {
+              const value = e.target.value;
+              //if (value === "" || /^([01]\d|2[0-3]):([0-5]\d)$/.test(value))
+              handleDurationChange(value);
+            }}
             disabled={!isEditing}
           />
         </div>
-        <div className="d-flex flex-column">
+        <div className="d-flex flex-column m-2">
           <button
             className={`btn btn-primary rounded-circle `}
             onClick={handleEditClick}
           >
-            {" "}
             <div hidden={isEditing}>
               <PencilSquare />
             </div>
@@ -174,15 +213,17 @@ const SchedulerItem: React.FC<SchedulerItemComponentProps> = ({
           </button>
         </div>
       </div>
-      {(localItem.mode === "weekly") && <div className="d-flex flex-column align-items-start mb-3 ">
-        <label className="mx-3"> Week Days: </label>
-        <WeekSelector
-          initialSelectedDays={item.weekday}
-          onSelectionChange={handleWeekSelectorChange}
-          isEnabled={isEditing}
-        />
-      </div>}
-      <div className="d-flex w-100">
+      {localItem.mode === "weekly" && (
+        <div className="d-flex flex-column align-items-start mb-3 ">
+          <label className="mx-3"> Week Days: </label>
+          <WeekSelector
+            initialSelectedDays={item.weekday}
+            onSelectionChange={handleWeekSelectorChange}
+            isEnabled={isEditing}
+          />
+        </div>
+      )}
+      <div className="d-flex w-100 p-1">
         <button className="btn btn-danger w-100 mx-1" onClick={handleRemove}>
           Remove
         </button>
@@ -194,19 +235,6 @@ const SchedulerItem: React.FC<SchedulerItemComponentProps> = ({
         >
           {item.enabled ? "Disable" : "Enable"}
         </button>
-      </div>
-      <div className={`d-flex align-items-center w-100 mb-1 `}>
-        <label className="text-muted w-auto"> Cooler Event: </label>
-        <EventPicker
-          value={localItem.skipEvent_id}
-          onChange={(id: number) => {
-            setLocalItem(prevItem => ({
-              ...prevItem,
-              skipEvent_id: id
-            }));
-          }}
-          disabled={!isEditing}
-        />
       </div>
     </div>
   );
